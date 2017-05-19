43449300894b6b7a056e52dcdceec397e5027e05
feuyomre <feuyomre@gmail.com>
2017-05-19 19:18:13 +0200

    Add files via upload

---
+++ sys.sh
@@ -0,0 +1,36 @@
+﻿#! /bin/bash
+
+echo "************************************"
+
+echo "Bienvenu sur mon projet sys "
+
+echo "************************************"
+
+
+echo "Choisir l'option à executer ........"
+
+echo "  
+ 1_L'information sur les utilisateurs enregistrés il y a trois jours "
+
+echo "  
+ 2_Acquisition, Installation, et Lancement de l'environnement XAMPP"
+
+echo "  
+ 3_Archivages des éléments du repertoire personnel qui ont été modifié par l'utilisateur SUDOER il y a deux jours dans un péripherique exterme "
+
+echo "   
+
+4_Information sur l'utilisateur du disque, de la mémoire, du processeur, et de la swap"
+
+echo "  
+ 5_quitter ( q ou Q)"
+
+
+
+//installer d'abord les librerie
+sudo apt-get install ia32-libs
+//installation
+sudo tar xvfz ./xampp-linux-*.tar.gz -C /opt
+
+
+sudo ./xampp-linux-*-installer.run
